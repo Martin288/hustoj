9d999c7a8ef94e157af885faadbaae90e50a0ce6
Martin <Martin.Hong@dji.com>
2016-04-29 14:46:20 +0800

    update install interative shell script

--- trunk/install/install-interactive.sh
+++ trunk/install/install-interactive.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-#before install check DB setting in 
-#	judge.conf 
-#	hustoj-read-only/web/include/db_info.inc.php
-#	and down here
-#and run this with root
 
-#CENTOS/REDHAT/FEDORA WEBBASE=/var/www/html APACHEUSER=apache 
 WEBBASE=/var/www/html
-APACHEUSER=www-data
+NGINXUSER=www-data
 DBUSER=root
 DBPASS=root
 
@@ -25,37 +19,22 @@ then
 fi
 
 #try install tools
-if uname -a|grep 'Ubuntu\|Debian'
-then 
-sudo apt-get install make flex g++ clang libmysql++-dev php5 apache2 mysql-server php5-mysql php5-gd php5-cli mono-gmcs subversion
-sudo /etc/init.d/mysql start
-HTTP_START="sudo /etc/init.d/apache2 restart"
-
-else
-sudo yum -y update
-sudo yum -y install php httpd php-mysql mysql-server php-xml php-gd gcc-c++  mysql-devel php-mbstring glibc-static flex
-sudo /etc/init.d/mysqld start
-WEBBASE=/var/www/html
-APACHEUSER=apache
-HTTP_START="sudo /etc/init.d/httpd restart"
-echo "/usr/bin/judged" > judged
-fi
+sudo apt-get install -y make flex g++ clang libmysql++-dev php5 php5-mysql php5-gd php5-cli mono-gmcs
 
 sudo svn checkout https://github.com/zhblue/hustoj/trunk/trunk hustoj-read-only
 
 #create user and homedir
-sudo  /usr/sbin/useradd -m -u 1536 judge
-
-
+sudo /usr/sbin/useradd -m -u 1536 judge
 
 #compile and install the core
 cd hustoj-read-only/core/
 sudo ./make.sh
-cd ../..
+cd ../../
+
 #install web and db
 sudo cp -R hustoj-read-only/web $WEBBASE/JudgeOnline
 sudo chmod -R 771 $WEBBASE/JudgeOnline
-sudo chown -R $APACHEUSER $WEBBASE/JudgeOnline
+sudo chown -R $NGINXUSER $WEBBASE/JudgeOnline
 sudo mysql -h localhost -u$DBUSER -p$DBPASS < db.sql
 
 #create work dir set default conf
@@ -69,9 +48,10 @@ sudo    mkdir /home/judge/run2
 sudo    mkdir /home/judge/run3
 sudo cp java0.policy  judge.conf /home/judge/etc
 sudo chown -R judge /home/judge
-sudo chgrp -R $APACHEUSER /home/judge/data
+sudo chgrp -R $NGINXUSER /home/judge/data
 sudo chgrp -R root /home/judge/etc /home/judge/run?
 sudo chmod 775 /home/judge /home/judge/data /home/judge/etc /home/judge/run?
+
 #update database account
 SED_CMD="s/OJ_USER_NAME=root/OJ_USER_NAME=$DBUSER/g"
 SED_CMD2="s/OJ_PASSWORD=root/OJ_PASSWORD=$DBPASS/g"
@@ -82,7 +62,6 @@ SED_CMD2="s/DB_PASS=\\\"root\\\"/DB_PASS=\\\"$DBPASS\\\"/g"
 
 sed $SED_CMD hustoj-read-only/web/include/db_info.inc.php|sed $SED_CMD2 >$WEBBASE/JudgeOnline/include/db_info.inc.php
 
-
 #boot up judged
 sudo cp judged /etc/init.d/judged
 sudo chmod +x  /etc/init.d/judged
@@ -90,7 +69,7 @@ sudo ln -s /etc/init.d/judged /etc/rc3.d/S93judged
 sudo ln -s /etc/init.d/judged /etc/rc2.d/S93judged
 sudo /etc/init.d/judged start
 
-$HTTP_START
+# $HTTP_START
 
 if uname -a | grep 'Ubuntu\|Debian'
 then
